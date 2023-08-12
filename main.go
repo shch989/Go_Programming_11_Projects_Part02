225b9042a6f9fef4721304c6f29f784c75b238b2
shch989 <tankpop11@gmail.com>
2023-08-13 00:57:56 +0900

    getMovies & deleteMovie 기능구현

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,10 +21,44 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
-var movie []Movie
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
 
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "438227",
+		Title: "Movie One",
+		Director: &Director{
+			Firstname: "John",
+			Lastname:  "Doe",
+		}})
+
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "45455",
+		Title: "Movie Two",
+		Director: &Director{
+			Firstname: "Steve",
+			Lastname:  "Smith",
+		}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
